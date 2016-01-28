88bef36d59089864728d61428ee5fb149a422e70
evesgf <evesgf@163.com>
2016-01-29 00:40:57 +0800

    add ignore list

--- Dev/NewBot/BLL/UsersBLL.cs
+++ Dev/NewBot/BLL/UsersBLL.cs
@@ -1,16 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NewBot.Common;
+using NewBot.Models;
 
 namespace NewBot.BLL
 {
     public class UsersBLL : IUsersBLL
     {
+        MySqlHelper nh=new MySqlHelper();
+
         public string Hello()
         {
-            return "Hello";
+            var sqlstr = "select * from users";
+            var dt=nh.ExecuteQuery(sqlstr, CommandType.Text);
+            var users=new UsersModel();
+            users.UserName= dt.Rows[0].ItemArray[1].ToString();
+            return users.UserName;
         }
     }
 }
